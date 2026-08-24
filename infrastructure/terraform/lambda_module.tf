30eab2afc7d7cf3341cb48c09fd3d3a4eae212ba
Tony <thitiklinpon@gmail.com>
2026-08-24 20:44:54 +0700

    fix: Fix comment

--- infrastructure/terraform/lambda_module.tf
+++ infrastructure/terraform/lambda_module.tf
@@ -9,6 +9,17 @@ module "lambda_create_transaction" {
   ]
 }
 
+module "lambda_get_transaction" {
+  source          = "./lambda/get_transaction"
+  lambda_role_arn = aws_iam_role.lambda_role.arn
+  table_name      = aws_dynamodb_table.dynamodb_table.name
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_basic_execution,
+    aws_iam_role_policy.lambda_dynamodb_access
+  ]
+}
+
 module "lambda_list_transactions" {
   source          = "./lambda/list_transactions"
   lambda_role_arn = aws_iam_role.lambda_role.arn

c7eb0586562769c73f6b4d074e266d95b6415ce6
Tony <thitiklinpon@gmail.com>
2026-09-09 21:10:25 +0700

    chore: Fix trailing comma

--- app/transaction/transaction.go
+++ app/transaction/transaction.go
@@ -63,7 +63,7 @@ func NewTransaction(walletID string, walletName string, categoryID string, categ
 		GSI_ByCategoryPK: "USER#" + ownerID + "#TX_CATEGORY#" + categoryID,
 		GSI_ByCategorySK: "TX#" + dateStr + "#" + id,
 		GSI_ByWalletPK:   "USER#" + ownerID + "#TX_WALLET#" + walletID,
-		GSI_ByWalletSK:   "TX#" + dateStr + "#" + id
+		GSI_ByWalletSK:   "TX#" + dateStr + "#" + id,
 		ID:               id,
 		WalletID:         walletID,
 		WalletName:       walletName,
